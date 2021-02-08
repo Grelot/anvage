c0c57aa48205b186ef5e82695ca1659bef0bd379
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2021-02-08 13:31:22 +0100

    select cmd args

--- anvage/arguments.py
+++ anvage/arguments.py
@@ -26,21 +26,29 @@ def parse_args(usage=HELPER_TEXT):
 
     subprasers = parser.add_subparsers(dest='command')
 
+    select = subprasers.add_parser('select', help='extract genetic variants located on the selected type of annotated regions')
+    select.add_argument("-f","--vcf", type=str, help='path of the variant Calling File (VCF) with variants you want to select', required=True)
+    select.add_argument("-g","--genome", type=str, help='path of the genome sequences FASTA file', required=True)
+    select.add_argument("-a","--annotation",type=str, help='path of the genome annotation GFF3 file', required=True)
+    select.add_argument("-s","--selectionAnnotationType", type=str, help='GFF3 feature type column used to select a variant (e.g. "exon")', required=True)
+    select.add_argument("-o","--output_prefix", type=str, help='prefix of the output VCF such as [PREFIX]_[selectionAnnotationType].vcf')
+
     synonymous = subprasers.add_parser('synonymous', help='detect synonymous genetic variants')
     synonymous.add_argument("-o","--output_prefix", type=str, help='prefix of the two output VCF files such as [PREFIX]_synonymous.vcf and [PREFIX]_nonsynonymous.vcf')
     synonymous.add_argument("-f","--vcf", type=str, help='path of the variant Calling File (VCF) with variants you want to determine synonymous or non-synonymous', required=True)
     synonymous.add_argument("-g","--genome", type=str, help='path of the genome sequences FASTA file', required=True)
     synonymous.add_argument("-a","--annotation",type=str, help='path of the genome annotation GFF3 file', required=True)
 
-    flank = subprasers.add_parser('flank', help='extract SNPs flanking sequences based on coordinates')
+    flank = subprasers.add_parser('flank', help='extract genetic variants flanking sequences')
     flank.add_argument("-f","--vcf", type=str, help='path of the variant Calling File (VCF) with variants from which you want to extract genome sequences')
     flank.add_argument("-g","--genome", type=str, help='path of the genome sequences FASTA file')
     flank.add_argument("-w","--windowsSize", type=int, help='size of window centered on position of variant to extract sequences from the genome')
     flank.add_argument("-o","--output_prefix", type=str, help='prefix of the output FASTA file such as [PREFIX]_.flanking.fasta')
-    
+
+   
     args = parser.parse_args()
 
-    if args.command not in ['synonymous', 'flank']:
+    if args.command not in ['select', 'synonymous', 'flank']:
         print(usage)
         sys.exit(0)
 
